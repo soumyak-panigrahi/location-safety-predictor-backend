761b2d19ae2d2bc336466af5a0af1deb9e8c9cbc
Aranyak Panigrahi <Aranyak Panigrahi>
2023-11-11 16:40:59 +0530

    initial version

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # location-safety-predictor-backend
+
+pip install virtualenv
+python -m virtualenv env
+source env/Scripts/activate
+
+pip install flask
+
+http://127.0.0.1:8000/sentiment_score?long=10&lat=20&time=30
---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, make_response, request, abort, jsonify, render_template
+#from api import make_soft_prediction, transform_input
+#import numpy as np
+
+app = Flask(__name__)
+app.debug = True
+
+
+@app.route('/sentiment_score', methods=['GET','POST'])
+def get_sentiment_score():
+    score = 90
+    inputs = ''
+    if request.method == 'GET':
+        args = request.args
+        # Initialize default values
+        long,lat,time = 0,0,0
+        long,lat,time = args['long'],args['lat'],args['time']
+        print(long,lat,time)
+    else:
+        if not request.json:
+            abort(400)
+
+        inputs = request.get_json()
+        long,lat,time = inputs['long'],inputs['lat'],inputs['time']
+        print(long,lat,time)
+
+    #inputs = list(map(float, inputs))
+    # score = make_soft_prediction(inputs)
+    # score = round(float(score),2)
+    response = {
+        'review': inputs,
+        'score': score
+    }
+
+    print("response is: {}".format(response))
+
+    return jsonify(response), 201
+
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+    
+if __name__ == '__main__':
+    app.run(debug=True,port=8000)
