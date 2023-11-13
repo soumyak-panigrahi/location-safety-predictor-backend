64ffa7c2bb44325c7870579d17c0e4f424e063e5
Soumyak Panigrahi <soumyakpanigrahi123@gmail.com>
2023-11-13 23:58:25 +0530

    Added Prediction Code

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, make_response, request, abort, jsonify, render_template
 from flask_cors import CORS
-#from api import make_soft_prediction, transform_input
+from prediction import safety_score, most_possible_crime
 #import numpy as np
 
 app = Flask(__name__)
@@ -15,32 +15,32 @@ def get_sentiment_score():
     if request.method == 'GET':
         args = request.args
         # Initialize default values
-        long,lat,time = 0,0,0
-        long,lat,time = args['long'],args['lat'],args['time']
-        print(long,lat,time)
+        long,lat,hour,minute = 0,0,0,0
+        long,lat,hour, minute = args['long'],args['lat'],args['hour'],args['minute']
+        print(long,lat,hour, minute)
     else:
         if not request.json:
             abort(400)
 
         inputs = request.get_json()
-        long,lat,time = inputs['long'],inputs['lat'],inputs['time']
-        print(long,lat,time)
+        long,lat,hour,minute = inputs['long'],inputs['lat'],inputs['hour'],inputs['minute']
+        print(long,lat,hour, minute)
 
-    #inputs = list(map(float, inputs))
-    # score = make_soft_prediction(inputs)
-    # score = round(float(score),2)
+    input = [ hour, lat, long, minute]
+    
+    inputs = list(map(float, inputs))
+    score = safety_score(inputs)
+    score = round(float(score),2)
+    crime = most_possible_crime(inputs)
     response = {
         'review': inputs,
-        'score': score
+        'score': score,
+        'crime': crime
     }
 
     print("response is: {}".format(response))
 
     return jsonify(response), 201
-
-# @app.route('/')
-# def index():
-#     return render_template('index.html')
     
 if __name__ == '__main__':
     app.run(debug=True,port=8000)
